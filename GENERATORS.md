8bd1948e4ad5ba65322871f735a9c2c5a0eaa2e2
Mario Amaral <mario.fts@gmail.com>
2018-08-08 19:31:14 -0300

    after generator

---
+++ GENERATORS.md
@@ -0,0 +1,8 @@
+
+Project generator
+`mix phx.new rumbl --database mysql`
+
+Managing Users
+`mix phx.gen.html Accounts User users name:string username:string password:string password_hash:string --web Accounts`
+
+
--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -2,21 +2,28 @@ defmodule RumblWeb.Router do
   use RumblWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
+  end
+
+  scope "/accounts", RumblWeb.Accounts, as: :accounts do
+    pipe_through(:browser)
+    ...
+    resources("/users", UserController)
   end
 
   scope "/", RumblWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
