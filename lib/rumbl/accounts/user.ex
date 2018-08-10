9c71b98978e036ef41649937708149615473b9a3
Mario Amaral <mario.fts@gmail.com>
2018-08-09 23:21:49 -0300

    Added password encryption and fixes some generates forms

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -14,8 +14,20 @@ defmodule Rumbl.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :username, :password, :password_hash])
+    |> cast(attrs, [:name, :username, :password])
     |> validate_length(:username, min: 1, max: 20)
-    |> validate_required([:name, :username])
+    |> validate_length(:password, min: 6, max: 200)
+    |> validate_required([:name, :username, :password])
+    |> put_pass_hash
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
